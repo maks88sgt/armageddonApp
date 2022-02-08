a634a1404890ce09660d180a3793472feae06176
Lera Abramova <67120537+Vareria-s@users.noreply.github.com>
2022-02-08 14:56:14 +0500

    api

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,13 +1,47 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState,} from 'react';
 import Filter from './Filter/Filter';
 import Footer from "./Footer/Footer";
 import {Route, Routes} from "react-router-dom";
 import Destruction from "./Cards/destruction/Destruction";
 import Card from "./Cards/Card";
 
-const Content = ({asteroids}) => {
+
+const Content = () => {
     const [isDangerous, setIsDangerous] = useState(false);
     const [isDistance, setIsDistance] = useState(true);
+
+    const [asteroids, setAsteroids] = useState([]);
+
+    const dateObj = new Date();
+    const day = dateObj.getUTCDate();
+    const month = dateObj.getMonth() + 1; //months from 1-12
+    const year = dateObj.getUTCFullYear();
+
+
+    useEffect(()=>{
+        fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-02-06&end_date=2022-02-08&api_key=DEMO_KEY')
+            .then(response => response.json())
+                .then(data => {
+                    let asteroids = [];
+                    const nearEarthObjects = data.near_earth_objects;
+                    for (let day in nearEarthObjects) {
+                        asteroids = asteroids.concat(nearEarthObjects[day])
+                    }
+                    setAsteroids(asteroids.map(asteroid => {
+                        return {
+                            name: asteroid.name,
+                            date: asteroid.close_approach_data[0].close_approach_date,
+                            distance: {
+                                kilometers: asteroid.close_approach_data[0].miss_distance.kilometers,
+                                moon: asteroid.close_approach_data[0].miss_distance.lunar,
+                            },
+                            size: asteroid.estimated_diameter.kilometers.estimated_diameter_max,
+                            inDangerous: asteroid.is_potentially_hazardous_asteroid,
+                        }
+                    }));
+                });
+    }, [])
+
 
     return (
         <div>
@@ -18,7 +52,7 @@ const Content = ({asteroids}) => {
                     setIsDistance={setIsDistance}/>
             <div className='menu-block'>
                 <Routes>
-                    <Route path="/" element={<Card  asteroids={asteroids}
+                    <Route path="/armageddonApp" element={<Card  asteroids={asteroids}
                                                     showDangerous={isDangerous}
                                                     isDistance={isDistance}/>}/>
                     <Route path="/asteroids" element={<Card  asteroids={asteroids}
